bf86d155aed44daf86cde88d8188cb139b6ca6ee
miku <svartkatt39@gmail.com>
2020-10-01 18:46:14 +0900

    get all info

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Form from "./components/Form.jsx";
 import axios from "axios";
 
 function App() {
-  async function test() {
-    let req = axios.get("http://localhost:5000/api/v1/restaurants/1");
-    let res = await req;
-    let data = res.data;
-    console.log(data);
-  }
-  test();
+  //   async function test() {
+  //     let req = axios.get("http://localhost:5000/api/v1/restaurants/1");
+  //     let res = await req;
+  //     let data = res.data;
+  //     console.log(data);
+  //   }
+  //   test();
 
   return (
     <div className="App wrapper">
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,33 +4,44 @@ import axios from "axios";
 
 //list restaruant names
 function List() {
+  //get all restaurant info
+  const [allInfo, setAllInfo] = useState("");
+  async function getAllInfo() {
+    let req = axios.get("http://localhost:5000/api/v1/restaurants/");
+    let res = await req;
+    let data = res.data;
+    console.log(JSON.stringify(data));
+
+    setAllInfo(JSON.stringify(data));
+  }
+
   return (
     <div>
       <h2>get restaurant info</h2>
-      <form>
-        <ul>
-          <li>
-            <label>
-              <span>get all restaurant info</span>
-              <button id="get_all_restaurant">all</button>
-            </label>
-          </li>
-          <li>
-            <label>
-              <span>get single restaurant info</span>
-              <input
-                type="text"
-                name="get_single_info"
-                placeholder="restaurant id?"
-              ></input>
-              <button id="get_single_restaurant">signle</button>
-            </label>
-          </li>
-        </ul>
-        <div className="card_base">
-          <div className="card">card</div>
-        </div>
-      </form>
+      <ul>
+        <li>
+          <label>
+            <span>get all restaurant info</span>
+            <button id="get_all_restaurant" onClick={getAllInfo}>
+              all
+            </button>
+          </label>
+        </li>
+        <li>
+          <label>
+            <span>get single restaurant info</span>
+            <input
+              type="text"
+              name="get_single_info"
+              placeholder="restaurant id?"
+            ></input>
+            <button id="get_single_restaurant">signle</button>
+          </label>
+        </li>
+      </ul>
+      <div className="card_base">
+        <div className="card">{allInfo}</div>
+      </div>
     </div>
   );
 }
