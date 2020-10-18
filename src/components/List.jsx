a4ecdbf3771c5869b48c027d5ee29a5b416d79a3
miku <svartkatt39@gmail.com>
2020-10-18 23:35:50 +0900

    test

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,29 +11,29 @@ function List() {
   const [singleInfo, setSingleInfo] = useState("");
 
   //get all restaurant info is rendered everytime when this page is rendered 
-  useEffect(() => {
-    async function getAllInfo() {
-      let req = axios.get("https://cc14polyglottal-app.herokuapp.com/api/v1/restaurants/");
-      let res = await req;
-      let data = res.data;
-      console.log(data);
-      let temp = [];
-      for (let key in data) {
-        console.log(data[key]);
-        temp.push(data[key]);
-      }
-      console.log(temp);
-      setAllInfo(
-        temp.map((el) => {
-          return (
-            <ul>
-              <li>ID: {el[1]}, Name: {el[0]}, Feature: {el[3]}</li>
-            </ul>
-          );
-        })
-      );
-    }
-    getAllInfo()}, [])
+  // useEffect(() => {
+  //   async function getAllInfo() {
+  //     let req = axios.get("https://cc14polyglottal-app.herokuapp.com/api/v1/restaurants/");
+  //     let res = await req;
+  //     let data = res.data;
+  //     console.log(data);
+  //     let temp = [];
+  //     for (let key in data) {
+  //       console.log(data[key]);
+  //       temp.push(data[key]);
+  //     }
+  //     console.log(temp);
+  //     setAllInfo(
+  //       temp.map((el) => {
+  //         return (
+  //           <ul>
+  //             <li>ID: {el[1]}, Name: {el[0]}, Feature: {el[3]}</li>
+  //           </ul>
+  //         );
+  //       })
+  //     );
+  //   }
+  //   getAllInfo()}, [])
 
 
   //get id from placeholder
