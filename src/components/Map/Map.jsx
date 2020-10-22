be2130a6f31996366585ed95c37abd521ebcd420
miku <svartkatt39@gmail.com>
2020-10-22 20:17:57 +0900

    change map size

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { GoogleMap } from '@react-google-maps/api';
+import axios from "axios"; 
 
 export default function Map(){
+
   
     const mapStyles = {
         margin: "0 auto",
-        height: "80vh",
+        height: "50vh",
         width: "70%",
         boxShadow: "0 4px 8px grey",
+        marginTop: "30px",
         marginBottom: "50px",
         borderRadius: "30px"
       };
@@ -17,13 +20,36 @@ export default function Map(){
         lat: 35.658584 ,
         lng: 139.7454316,
       };
+
+        async function mapAddresses() {
+          //get addresses from restaurants 
+            let req = axios.get("https://cc14polyglottal-app.herokuapp.com/api/v1/restaurants/");
+            let res = await req;
+            let data = res.data;
+            let temp = [];
+            for (let key in data) {
+              temp.push(data[key]);
+            }
+            let addresses = temp.map((el) => {
+                return (
+                    el[3]
+                );
+                })
+              console.log(addresses)
+              //I'd like to get lat and lng from addresses here 
+              //use google geocorder 
+
+
+            }
+
+        mapAddresses(); 
       
       return (
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={13}
+          zoom={12}
           center={defaultCenter}
         />
   )
-}
+};
 
